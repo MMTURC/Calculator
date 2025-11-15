f0a543fdeec8f6b99dc9079acb181ebb9a09a8ca
Mauricio <111543689+MMTURC@users.noreply.github.com>
2025-11-15 11:45:14 -0500

    Added Basic Functionality for getting user input and prompting the user for numbers.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Test");
+﻿int numberOne = 0;
+
+int numberTwo = 0;
+
+string answer;
+
+Console.WriteLine("This is a calculator\r");
+Console.WriteLine("--------------------\n");
+
+Console.WriteLine("Please type a number: ");
+
+numberOne = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please type a second number: ");
+
+numberTwo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Choose what operation you would like!");
+Console.WriteLine("Would you like to add, subtract, multiply and divide");
+
+answer = Console.ReadLine();
+
+
+
+switch (answer)
+{
+
+}
+
+
