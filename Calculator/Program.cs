cbf365f7219392f5182daf8be59e97562190f514
Mauricio <111543689+MMTURC@users.noreply.github.com>
2025-11-15 12:04:03 -0500

    Put the whole thing a while loop to keep prompting the user if they want to use more operations.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,42 +1,50 @@
-﻿int numberOne = 0;
-
-int numberTwo = 0;
-
-string answer;
-
+﻿
 Console.WriteLine("This is a calculator\r");
 Console.WriteLine("--------------------\n");
 
-Console.WriteLine("Please type a number: ");
-
-numberOne = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Please type a second number: ");
-
-numberTwo = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Choose what operation you would like!");
-Console.WriteLine("Would you like to add, subtract, multiply and divide?");
-
-
-answer = Console.ReadLine();
-
-
-// ToLower handles if the user does not match the case exactly just the word.
-switch (answer.ToLower())
+while (true)
 {
-    case "add":
-        Console.WriteLine($"Your result is {numberOne}  + {numberTwo} = " + (numberOne + numberTwo));
-        break;
-    case "subtract":
-        Console.WriteLine($"Your result is {numberOne} - {numberTwo} = " + (numberOne - numberTwo));
-        break;
-    case "multiply":
-        Console.WriteLine($"Your result is {numberOne} * {numberTwo} = " + (numberOne * numberTwo));
-        break;
-    case "divide":
-        Console.WriteLine($"Your result is {numberOne} / {numberTwo} = " + (numberOne / numberTwo));
-        break;
+
+
+    double numberOne = 0;
+
+    double numberTwo = 0;
+
+    string answer;
+
+    Console.WriteLine("Please type a number: ");
+
+    numberOne = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Please type a second number: ");
+
+    numberTwo = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Choose what operation you would like!");
+    Console.WriteLine("Would you like to add, subtract, multiply and divide?");
+
+
+    answer = Console.ReadLine();
+
+    switch (answer.ToLower())
+    {
+        case "add":
+            Console.WriteLine($"Your result is {numberOne}  + {numberTwo} = " + (numberOne + numberTwo));
+            break;
+        case "subtract":
+            Console.WriteLine($"Your result is {numberOne} - {numberTwo} = " + (numberOne - numberTwo));
+            break;
+        case "multiply":
+            Console.WriteLine($"Your result is {numberOne} * {numberTwo} = " + (numberOne * numberTwo));
+            break;
+        case "divide":
+            Console.WriteLine($"Your result is {numberOne} / {numberTwo} = " + (numberOne / numberTwo));
+            break;
+    }
+    Console.WriteLine("You can now perform another operation if you would like if not do Ctrl + c on your keyboard (I am too lazy to add something that quits the program)\n");
+
 }
 
 
+
+// ToLower handles if the user does not match the case exactly just the word.
